45dcc59f7cc96c9a54104bbc1c7e3b9db68368e6
Eder2022 <eder@reinventargesso.com.br>
2022-08-09 15:09:18 -0300

    typscript-inicio

---
+++ build/corFavorita.js
@@ -0,0 +1,8 @@
+"use strict";
+const cor1 = prompt("Insira sua primeira cor favorita");
+const cor2 = prompt("Insira sua segunda cor favorita");
+const cor3 = prompt("Insira sua terceira cor favorita");
+function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
+    return ([cor1, cor2, cor3]);
+}
+//# sourceMappingURL=corFavorita.js.map
